92f929e59465c3b0f82499ecd1afd28d3c215879
surajyadav471 <115660802+surajyadav471@users.noreply.github.com>
2023-05-19 12:38:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+    event.preventDefault(); // prevent form from submitting
+        const firstName = document.getElementById("firstName").value;
+        const lastName = document.getElementById("lastName").value;
+        alert(`First Name: ${firstName}\nLast Name: ${lastName}`);
 }
